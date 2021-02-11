60e9a0c23fb7e03692e79e78fb5a8e74ae40cd37
ninoch <nazanin.alipourfard@gmail.com>
2021-02-10 20:42:16 -0800

    adding all the results and figures

--- run_all.sh
+++ run_all.sh
@@ -6,6 +6,9 @@ done
 
 for field in management political_science psychology economics aps cs
 do
-	echo "***** Running for affiliation $field 1990 *****"
-	python main.py --network_type=affiliation --field=$field --from_year=1990 --top=100 > logs/affiliation-top100-$field-1990.log
+	for top in 10 20 50 100
+	do
+		echo "***** Running for affiliation $field 1990 top-$top *****"
+		python main.py --network_type=affiliation --field=$field --from_year=1990 --top=$top > logs/affiliation-top$top-$field-1990.log
+	done
 done
